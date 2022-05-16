aab82e6b33efbd5c5b417cbe97838002979c74f8
Vivek <panchalvivek25@gmail.com>
2022-05-16 23:29:59 +0930

    bubble

--- main.cpp
+++ main.cpp
@@ -4,11 +4,47 @@
 
 #include <string>
 #include <vector>
-
+#include <sstream>
 
 int main(){
 
+    std::string in;
+    std::getline (std::cin, in);
+    std::stringstream stream(in);
+    std::vector<int> iList;
+
+    int n;
+
+    while (stream >> n) {
+        iList.push_back(n);
+    }
+
+    
+    BubbleSort bSorter = BubbleSort();
+    QuickSort qSorter = QuickSort();
+    RecursiveBinarySearch search1 = RecursiveBinarySearch();
+
+    std::vector<int> sorted = qSorter.sort(iList);
+    bool response = search1.search(sorted, 1);
+
+
+    if (response == true) {
+        std::cout << "true ";
+    }
+    else  {
+        std::cout << "false ";
+    }
+
+    for (int i = 0; i < sorted.size(); i++) {
+        if (i < sorted.size() - 1)  {
+            std::cout << sorted.at(i) << " ";
+        }
+        else  {
+            std::cout << sorted.at(i) << std::endl;
+        }
+    }
 
     return 0;
+
 
 }
